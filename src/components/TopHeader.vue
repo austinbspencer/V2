277ec448dab72641c4c5bfef4a00fb764b44e601
abspen1 <abspencer2097@gmail.com>
2020-09-20 16:39:48 -0700

    move folder

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="light" variant="info" fixed="top">
-      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-brand>Projects</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/">Home</b-nav-item>
+          <b-nav-item to="/projects">Projects</b-nav-item>
           <b-nav-item to="/about">About</b-nav-item>
         </b-navbar-nav>
 
