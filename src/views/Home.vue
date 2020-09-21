c991724620e724dd1d7e47c03019a738bd542d06
abspen1 <abspencer2097@gmail.com>
2020-09-21 02:30:04 -0700

    add gretchens image to home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,14 @@
 <template>
-  <div
-    class="w-full"
-    :style="{ backgroundImage: `url(https://vuejs.org/images/logo.png)` }"
-  >
-    <p> Hello </p>
-  
+  <div class="home">
+    <b-card
+      overlay
+      img-src="https://images.unsplash.com/photo-1600680386586-9b4795bee256?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+      img-alt="Card Image"
+      text-variant="dark"
+      title="Welcome To My Website"
+      sub-title="Austin Spencer"
+    >
+    </b-card>
   </div>
 </template>
 
