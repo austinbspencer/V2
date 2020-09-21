2551f7b9395ff7f8f0fbd8887635d59b2051ec9d
abspen1 <abspencer2097@gmail.com>
2020-09-21 03:39:56 -0700

    updating about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,27 +2,85 @@
   <div class="about">
     <b-container>
       <b-row>
-        <b-col cols="6">
+        <b-col cols="5">
           <b-card
-            img-top
+            overlay
             img-src="https://scontent.fphx1-1.fna.fbcdn.net/v/t1.0-9/83322175_2958884524156199_6029303686017056768_n.jpg?_nc_cat=111&_nc_sid=8bfeb9&_nc_ohc=fnfapXEthZMAX_avawZ&_nc_oc=AQlPmAdGcaj7aAR2YRr3Omt2C-MlqWsYSVQM6zG-rM1fDbjo_hKqeAjsHMFJ4CGU-_x3ASTBzSSLN9VL6PNGpfGZ&_nc_ht=scontent.fphx1-1.fna&oh=6281bba06014fd36955b4b3d1e86536b&oe=5F8CE9F3"
             img-alt="Card Image"
             text-variant="dark"
-            title="I am Austin Spencer"
-            sub-title="Thanks for visiting my website!"
+            style="min-width: 20rem;"
           >
           </b-card>
         </b-col>
         <b-col cols="2"></b-col>
-        <b-col cols="4">
-          <b-card text-variant="dark" title="A little about me...">
+        <b-col cols="5">
+          <b-card
+            text-variant="dark"
+            title="A little about me..."
+            footer-tag="footer"
+          >
             <b-card-text
-              >Hi, I'm Austin Spencer. Thank you for visiting my website! A
-              little bit about me –– I am an undergraduate student at Arizona
-              State University. My major is computer science and I plan to
-              graduate in May of 2022. I enjoy staying active and being
-              outdoors! If you have a project idea I would love to hear it!
+              >Hi, I'm Austin Spencer. Thank you for visiting my website! I am
+              an undergraduate student at Arizona State University. My major is
+              computer science and I plan to graduate in May of 2022. I enjoy
+              staying active and being outdoors! If you have a project idea I
+              would love to hear it!
             </b-card-text>
+            <!-- Footer template -->
+            <template v-slot:footer>
+              <!-- Button with custom close trigger value -->
+              <div class="w-100">
+                <b-button
+                  size="md"
+                  variant="outline-dark"
+                  class="float-left"
+                  href="https://github.com/abspen1"
+                >
+                  GitHub
+                </b-button>
+                <b-button
+                  size="md"
+                  variant="outline-dark"
+                  class="float-right"
+                  href="https://austinspencer.works/about/contact/"
+                >
+                  Contact
+                </b-button>
+              </div>
+            </template>
+          </b-card>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col md="6"></b-col>
+        <b-col md="4">
+          <b-card
+            img-top
+            img-src="https://pbs.twimg.com/profile_images/1283165293473419267/5TKLbwkL_400x400.jpg"
+            img-alt="Card Image"
+            text-variant="dark"
+            title="My Current Project"
+            style="min-width: 20rem;"
+            footer-tag="footer"
+          >
+            <div class="w-100">
+              <b-button
+                size="sm"
+                variant="outline-dark"
+                class="float-left"
+                href="https://github.com/abspen1/Fantasy-Twitter"
+              >
+                GitHub
+              </b-button>
+              <b-button
+                size="sm"
+                variant="outline-dark"
+                class="float-right"
+                href="https://austinspencer.works/FantasyTwitter/"
+              >
+                Webpage
+              </b-button>
+            </div>
           </b-card>
         </b-col>
       </b-row>
