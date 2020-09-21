b0d95f19de07835d04099f9f8f14097b182a605f
abspen1 <abspencer2097@gmail.com>
2020-09-21 14:17:44 -0700

    getting closer

--- src/components/TodoComponent.vue
+++ src/components/TodoComponent.vue
@@ -1,19 +1,44 @@
 <template>
   <div id="todo">
-    <b-card v-if="posts && posts.length">
-      <b-card-text v-for="(obj, key) in posts" :key="key">
-        <p>
-          <strong>{{ obj.title }}</strong>
-        </p>
-        <p>{{ obj.completed }}</p>
-      </b-card-text>
-    </b-card>
-
-    <ul v-if="errors && errors.length">
-      <template v-for="error of errors">
-        {{ error.message }}
-      </template>
-    </ul>
+      <b-row>
+          <b-col sm="2"></b-col>
+        <b-col sm="8">
+            <form>
+                <input type="text" name="title" placeholder="Add Todo...">
+                <input type="submit" value="Submit" class="btn">
+            </form>
+        </b-col>
+        <b-col sm="2"></b-col>
+      </b-row>
+    <b-row v-for="(obj, key) in posts" :key="key">
+      <b-col sm="2">
+        <template>
+            <b-form-checkbox
+            id="checkbox-1"
+            :name="obj.Title"
+            :checked="obj.Completed"
+            >
+            </b-form-checkbox>
+        </template>
+      </b-col>
+      <b-col sm="8">
+          <template v-if="obj.Completed == 'false'">
+              <p>
+                <strong> {{ obj.Title }} </strong>
+            </p>
+          </template>
+          <template v-else>
+              <p>
+                <strong>
+                    <strike> {{ obj.Title }} </strike>
+                </strong>
+            </p>
+          </template>
+      </b-col>
+      <b-col sm="2">
+          <button variant="danger">x</button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -29,10 +54,9 @@ export default {
 
   // Fetches posts when the component is created.
   created() {
-    axios
-      .get("https://guldentech.com/austinapi/todos")
-      .then(res => (this.posts = res.data))
-      .catch(err => console.log(err));
+    axios.get('https://guldentech.com/austinapi/todos')
+    .then(res => this.posts = res.data)
+    .catch(err => console.log(err))
 
     // async / await version (created() becomes async created())
     //
@@ -46,4 +70,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.del {
+        background: #ff0000;
+        color: #fff;
+        border: none;
+        padding: 5px 9px;
+        /* border-radius: 50%; */
+        cursor: pointer;
+        float: right;
+    }
+</style>
