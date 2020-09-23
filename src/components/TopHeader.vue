1d1a8928e5efc4e60e72e7cf03ef8490ea8fe963
abspen1 <abspencer2097@gmail.com>
2020-09-22 22:38:13 -0700

    colors!! 👍

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -10,11 +10,15 @@
       <b-navbar-brand>
         <b-row>
           <b-col cols="4" id="brand" v-b-tooltip.hover title="GitHub">
-            <a href="https://github.com/abspen1"><i class="fab fa-github"></i>&emsp;</a>
+            <a href="https://github.com/abspen1"
+              ><i class="fab fa-github"></i>&emsp;</a
+            >
             <span>&emsp;</span>
           </b-col>
           <b-col cols="4" id="brand" v-b-tooltip.hover title="LinkedIn">
-            <a href="https://www.linkedin.com/in/austin-spencer-b56a25177/"><i class="fab fa-linkedin-in"></i>&emsp;</a>
+            <a href="https://www.linkedin.com/in/austin-spencer-b56a25177/"
+              ><i class="fab fa-linkedin-in"></i>&emsp;</a
+            >
           </b-col>
         </b-row>
       </b-navbar-brand>
@@ -41,18 +45,25 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="https://austinspencer.works/about/contact/"
-            right><i class="fas fa-envelope"></i></b-nav-item
-          >
+          <b-nav-item id="mail-icon" right
+            ><a href="https://austinspencer.works/about/contact/"
+              ><i class="fas fa-envelope"></i></a
+          ></b-nav-item>
           <b-nav-item-dropdown text="Social" right>
-            <b-dropdown-item href="https://twitter.com/austinnspencer"
-              ><i class="fab fa-twitter"></i></b-dropdown-item
+            <b-dropdown-item
+              variant="info"
+              href="https://twitter.com/austinnspencer"
+              ><i class="fab fa-twitter"> Twitter</i></b-dropdown-item
             >
-            <b-dropdown-item href="https://www.facebook.com/austin.spencer.129/"
-              ><i class="fab fa-facebook"></i></b-dropdown-item
+            <b-dropdown-item
+              variant="primary"
+              href="https://www.facebook.com/austin.spencer.129/"
+              ><i class="fab fa-facebook-f"> Facebook</i></b-dropdown-item
             >
-            <b-dropdown-item href="https://instagram.com/austinspencer"
-              ><i class="fab fa-instagram"></i></b-dropdown-item
+            <b-dropdown-item
+              variant="warning"
+              href="https://instagram.com/austinspencer"
+              ><i class="fab fa-instagram"> Instagram</i></b-dropdown-item
             >
           </b-nav-item-dropdown>
         </b-navbar-nav>
@@ -64,10 +75,16 @@
 <script></script>
 
 <style lang="scss" scoped>
-#brand a{
-  color: #8E9195;
+#brand a {
+  color: #8e9195;
 }
 #brand a:hover {
   color: #c0c3c7;
+}
+#mail-icon a {
+  color: #918816;
+}
+#mail-icon a:hover {
+  color: #beb320;
 }
 </style>
