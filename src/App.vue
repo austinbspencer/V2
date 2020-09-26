270215c539c56b47a70a0f1273755c2ec4f0d4ef
abspen1 <abspencer2097@gmail.com>
2020-09-26 11:15:25 -0700

    add fonts and text-align

--- src/App.vue
+++ src/App.vue
@@ -29,6 +29,12 @@ export default {
 </script>
 
 <style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
 #nav {
   padding: 30px;
 
